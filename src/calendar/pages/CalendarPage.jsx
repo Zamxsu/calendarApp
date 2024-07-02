3fe2e60a221c0e85bb4d1ab886c2386acc71c39f
martin <martin.rubiano1234@gmail.com>
2024-07-02 13:55:38 -0500

    Escuchar eventos del calendario

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -6,6 +6,7 @@ import { addHours } from "date-fns";
 import { CalendarEvent, Navbar } from "../";
 
 import { localizer, getMessagesES } from "../../helpers";
+import { useState } from "react";
 
 const events = [{
   title : 'Cum del jefe',
@@ -20,6 +21,8 @@ const events = [{
 }]
 
 export const CalendarPage = () => {
+
+  const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'week' )
 
   const eventStyleGetter = ( event, start, end , isSelected ) => {
 
@@ -36,6 +39,19 @@ export const CalendarPage = () => {
 
 
   }
+  
+  const onDoubleClick = ( event )  => {
+    console.log({ doubleClick: event });
+  }
+
+  const onSelect = ( event )  => {
+    console.log({ click: event });
+  }
+
+  const onViewChanged = ( event )  => {
+    localStorage.setItem('lastView', event )
+    setLastView( event )
+  }
 
   return (
     <>
@@ -45,6 +61,7 @@ export const CalendarPage = () => {
         culture="es"
         localizer={ localizer }
         events={ events }
+        defaultView={ lastView }
         startAccessor="start"
         endAccessor="end"
         style={{ height: 'calc( 100vh - 80px)' }}
@@ -53,6 +70,9 @@ export const CalendarPage = () => {
         components={{
           event: CalendarEvent
         }}
+        onDoubleClickEvent={ onDoubleClick }
+        onSelectEvent={ onSelect }
+        onView={ onViewChanged }
       />
     </>
   );
