ba8785aa46122be8d169a7c2067198a86ae6474e
martin <martin.rubiano1234@gmail.com>
2024-08-23 14:28:45 -0500

    Axios configurar peticiones http

---
+++ src/api/calendarApi.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+import { getEnvVariables } from '../helpers/getEnvVariables'
+
+
+
+const { VITE_API_URl } = getEnvVariables()
+
+
+const calendarApi = axios.create({
+
+    baseURL: VITE_API_URl
+
+});
+
+// Todo: Configurar interceptores
+
+
+export default calendarApi;
