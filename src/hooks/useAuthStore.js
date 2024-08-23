5823cebb40c34540868b1b904cddb407ad4842d2
martin <martin.rubiano1234@gmail.com>
2024-08-23 16:03:03 -0500

    Mantener el estado de autenticacion

--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -52,6 +52,26 @@ export const useAuthStore = () => {
 
     }
 
+    const checkAuthToken = async() => {
+
+        const token = localStorage.getItem('token')
+        if ( !token ) return dispatch( onLogout() );
+
+        try {
+            
+            const { data } =  await calendarApi.get('auth/renew')
+            localStorage.setItem('token', data.token )
+            localStorage.setItem('token-init-date', new Date().getTime() );
+            dispatch( onLogin({ name: data.name, uid: data.uid }))
+            
+        } catch (error) {
+            localStorage.clear();
+            dispatch( onLogout() );
+
+        }
+
+    }
+
     return {
         //* Propiedades
         status,
@@ -59,7 +79,8 @@ export const useAuthStore = () => {
         errorMessage,
         //! Metodos
         startLogin,
-        startRegister
+        startRegister,
+        checkAuthToken,
     }
 
 
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,23 +1,38 @@
-import { Navigate, Route, Routes } from "react-router-dom"
-import { LoginPage } from "../auth"
-import { CalendarPage } from "../calendar"
-
+import { Navigate, Route, Routes } from "react-router-dom";
+import { LoginPage } from "../auth";
+import { CalendarPage } from "../calendar";
+import { useAuthStore } from "../hooks/useAuthStore";
+import { useEffect } from "react";
 
 export const AppRouter = () => {
+  const { status, checkAuthToken } = useAuthStore();
 
-     const authStats =  'not authenticated'  //'not authenticated' 
+  useEffect(() => {
+
+    checkAuthToken();
+
+  }, []);
+
+  if (status === "checking") {
+
+    return (
+
+      <h3>Cargando...</h3>
+
+    )
+    
+  }
+
+  //  const authStats =  'not authenticated'  //'not authenticated'
 
   return (
     <Routes>
-        {
-            (
-                (authStats === 'not authenticated')
-                    ?  <Route path="/auth/*" element={ <LoginPage /> } />
-                    :  <Route path="/*" element={ <CalendarPage /> } />
-                
-            )
-        }
-        <Route path="/*" element={ <Navigate to='/auth/login' />  } />
+      {status === "not authenticated" ? (
+        <Route path="/auth/*" element={<LoginPage />} />
+      ) : (
+        <Route path="/*" element={<CalendarPage />} />
+      )}
+      <Route path="/*" element={<Navigate to="/auth/login" />} />
     </Routes>
-  )
-}
+  );
+};
