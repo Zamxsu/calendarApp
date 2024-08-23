c518e1f4f7968526525670e62f511838adb79ede
martin <martin.rubiano1234@gmail.com>
2024-08-23 16:17:01 -0500

    Comparando codigo

--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -53,28 +53,23 @@ export const useAuthStore = () => {
     }
 
     const checkAuthToken = async() => {
-
-        const token = localStorage.getItem('token')
+        const token = localStorage.getItem('token');
         if ( !token ) return dispatch( onLogout() );
 
         try {
-            
-            const { data } =  await calendarApi.get('auth/renew')
-            localStorage.setItem('token', data.token )
+            const { data } = await calendarApi.get('auth/renew');
+            localStorage.setItem('token', data.token );
             localStorage.setItem('token-init-date', new Date().getTime() );
-            dispatch( onLogin({ name: data.name, uid: data.uid }))
-            
+            dispatch( onLogin({ name: data.name, uid: data.uid }) );
         } catch (error) {
             localStorage.clear();
             dispatch( onLogout() );
-
         }
-
     }
 
     const startLogout = () => {
-        localStorage.clear
-        dispatch( onLogout() )
+        localStorage.clear();
+        dispatch(onLogout());
     }
 
     return {
