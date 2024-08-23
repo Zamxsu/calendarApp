d1390c2d63e3c33b4de0befa5a2046a1e1a94ab8
martin <martin.rubiano1234@gmail.com>
2024-08-23 15:06:59 -0500

    Despachar acciones respectivas

--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from "react-redux"
 import { calendarApi } from '../api'
+import { clearErrorMessage, onChecking, onLogin, onLogout } from "../store/auth/authSlice";
 
 
 export const useAuthStore = () => {
@@ -9,16 +10,21 @@ export const useAuthStore = () => {
 
     const startLogin = async({ email, password }) => {
 
-        console.log({ email, password });
-        
+        dispatch( onChecking() );
+
         try {
             
-            const resp = await calendarApi.post('/auth', { email, password })
-            console.log({ resp });
+            const { data } = await calendarApi.post('/auth', { email, password });
+            localStorage.setItem('token', data.token);
+            localStorage.setItem('token-init-date', new Date().getTime() );
+            dispatch( onLogin({ name: data.name, uid: data.uid }))
             
         } catch (error) {
-            
-            console.log({ error });
+
+            console.log( onLogout('Revise las credenciales') );
+            setTimeout(() => {
+                dispatch( clearErrorMessage() )
+            }, 10);
         
         }
 
--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -8,14 +8,22 @@ export const authSlice = createSlice({
        errorMessage: undefined
    },
    reducers: {
-       checking: ( state ) => {
-            state.status = 'checking',
-            state.user = {},
-            state.errorMessage = undefined
+       onChecking: ( state ) => {
+            state.status = 'checking';
+            state.user = {};
+            state.errorMessage = undefined;
        },
        onLogin: ( state, { payload } ) => {
-            state.status = 'authenticated',
-            state.user = payload,
+            state.status = 'authenticated';
+            state.user = payload;
+            state.errorMessage = undefined;
+       },
+       onLogout: ( state, { payload } ) => {
+            state.status = 'not-authenticated';
+            state.user = {};
+            state.errorMessage = payload;
+       },
+       clearErrorMessage: ( state ) => {
             state.errorMessage = undefined
        }
        
@@ -24,4 +32,4 @@ export const authSlice = createSlice({
 
 
 // Action creators are generated for each case reducer function
-export const { checking, onLogin } = authSlice.actions;
+export const { onChecking, onLogin, onLogout, clearErrorMessage } = authSlice.actions;
