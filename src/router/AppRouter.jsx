f0d338282f00cab17d2a773bc56f982a7f562c83
martin <martin.rubiano1234@gmail.com>
2024-08-26 08:24:46 -0500

    Quitando depedencias

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Navigate, Route, Routes } from "react-router-dom";
 import { LoginPage } from "../auth";
 import { CalendarPage } from "../calendar";
